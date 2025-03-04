6e1dd59f6ff960d60e42b1781ec27363aabf0d91
Khurram  Shams <141557450+KhurramShams@users.noreply.github.com>
2025-03-04 03:27:17 -0800

    Update clients.py

--- StreamLit/views/clients.py
+++ StreamLit/views/clients.py
@@ -7,29 +7,29 @@ col1,col2,col3 = st.columns(3,gap="small",vertical_alignment="top")
 
 with col1:
     st.markdown("**Fly Easy Travel and Tours Pvt Ltd**") 
-    st.image("assets/Flyeasy.png",width=150)
+    st.image("StreamLit/assets/Flyeasy.png",width=150)
     
 with col2:
     st.markdown("**Mirpurkhas Testing Service**") 
-    st.image("assets/MTS.png",width=80)
+    st.image("StreamLit/assets/MTS.png",width=80)
    
    
 with col3:
     st.markdown("**Regal Elite (E-Commerce)**") 
-    st.image("assets/RegalElite.png",width=80)
+    st.image("StreamLit/assets/RegalElite.png",width=80)
     st.write("")
 
 col4,col5,col6 = st.columns(3,gap="small",vertical_alignment="top")
 
 with col4:
     st.markdown("**Margalla Trail Runners**") 
-    st.image("assets/logo.png",width=150)
+    st.image("StreamLit/assets/logo.png",width=150)
     
 with col5:
     st.markdown("**Sky Trips Travel and Tours**") 
-    st.image("assets/SkyTrips.png",width=150)
+    st.image("StreamLit/assets/SkyTrips.png",width=150)
     
 with col6:
     st.markdown("**Mirpurkhas Mangoes**")
-    st.image("assets/MM.png",width=100)
+    st.image("StreamLit/assets/MM.png",width=100)
     
